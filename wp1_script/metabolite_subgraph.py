cb74da6f8bffa16cc7323521d132e5ab48650890
Paul Keller <contact.keller.paul@gmail.com>
2023-02-06 17:23:45 +0100

    added comments

--- wp1_script/metabolite_subgraph.py
+++ wp1_script/metabolite_subgraph.py
@@ -4,74 +4,152 @@ import pickle
 
 
 
-def prev_nodes(graph: nx.DiGraph, node: any, direction = True):
-    if direction:
-        return graph.predecessors(node)
-    else:
-        return graph.successors(node)
-def next_nodes( graph: nx.DiGraph, node, direction = True):
-    if direction:
-        return graph.successors(node)
-    else:
-        return graph.predecessors(node)
-
-
 def generate_test_graph():
+    """Generates a Test Graph, described in 'Example_Graph.png'."""
     G = nx.DiGraph()
-    G.add_nodes_from([1,2,3,4,5,6,7,8,9,10,11,12,13], type=False)
-    G.add_nodes_from(["R1", "R2", "R3","R4"], type=True)
+    # type = False means compound node
+    # type = True means reaction node
+    # this type annotation is not used later on
+    G.add_nodes_from([1,2,3,4,5,6,7,8,9,10,11,12,13,19], type=False)
+    G.add_nodes_from(["R1", "R2", "R3","R4", "R5"], type=True)
+    # direction = 1 is default direction
+    # direction = 2 is reversed direction
     G.add_edges_from([(1,"R1"),(2,"R1"),(3,"R1"),("R1",4),("R1",5),("R1",6)], direction=1)
     G.add_edges_from([(4,"R2"),(5,"R2"),("R2",7)], direction=1)
     G.add_edges_from([(7,"R2"),("R2",4),("R2",5)], direction=2)
     G.add_edges_from([(6, "R3"),(8,"R3"),("R3",10),("R3",9)], direction=1)
     G.add_edges_from([(12,"R4"),(13,"R4"),("R4",8),("R4",11)], direction=1)
-
+    G.add_edges_from([(11,"R5"),(19,"R5"),("R5",6)], direction=1)
     return G
 
-def add_reactions(queue: list[any], visited: set[any], reactions: set[any], startingNode: any, graph: nx.DiGraph, direction=True):
-    for reaction in reactions:
-        direction_label =graph.edges[startingNode, reaction]['direction']
-        conditions = [c for c in prev_nodes(graph, reaction, direction) if graph.edges[c, reaction]['direction'] == direction_label]
+def bf_search(graph: nx.DiGraph, start_node: any, essential_compounds: list[any], direction=True):
+    """Performs Breath First Search modified for Hyper Graphs.
+    graph = given Graph to perform BFS on
+    start_node = starting node
+    essential_compounds = list of compounds assumed as 'given'"""
 
-        if set(conditions).issubset(visited) and reaction not in visited and reaction not in queue:
-            queue.append(reaction)
-
-def bf_traverse(graph: nx.DiGraph, startNode, essentialCompounds, direction=True):
     # mark all essential nodes and starting node as visited
-    visited = set(essentialCompounds)
-    visited.add(startNode)
-    print(visited)
+    # visited is a set, since no node can be visited twice
+    # once seen, a node can always be reproduced by that reaction
+    visited = set(essential_compounds)
+    visited.add(start_node)
+    # initialize queue as list (since sets do not keep location)
     queue = []
 
-    # initialize queue
-    for startingNode in visited:
-        possible_reactions = next_nodes(graph, startingNode, direction)
-        add_reactions(queue, visited, possible_reactions, startingNode, graph, direction)
+    # for each compound node in our starting position
+    for node in visited:
+        # find all possible reactions this compound is part of
+        possible_reactions = next_nodes(graph, node, direction)
+        # add reaction nodes to the queue
+        # if the reaction has only educts, that are in the visited set.
+        add_reactions(queue, visited, possible_reactions, node, graph, direction)
 
     while queue:
-        # dequeue current hyper edge (reaction)
+        # dequeue current reaction
         current = queue.pop(0)
-        print(f"Queue: {len(queue)}")
-        print(f"Dequeu {len(visited)}")
-        # add current hyper edge to visited
+        # Debug output
+        # print(f"Queue: {len(queue)}")
+        # print(f"Dequeu {len(visited)}")
+        # add current reaction to visited (as in, this reaction is now performed)
         visited.add(current)
-        #enqueue
+        # for each product of this reaction
         for successor in next_nodes(graph, current, direction):
+            # add product compound to visited
             visited.add(successor)
-            # find all hyperedges of the products
+            # find all reactions this product is part of as an educt
             successor_reactions = next_nodes(graph, successor, direction)
-            # add hyperedge to possible reactions
+            # add reaction nodes to the queue
+            # if reactions has only educts, that are in the visited set
+            # and are not already visited
+            # and are not already in queue
             add_reactions(queue, visited, set(successor_reactions), successor, graph, direction)
+    # return the subgraph made by all visited nodes
     return graph.subgraph(visited)
 
-def main():
-    testGraph = generate_test_graph()
-    graph = pickle.load(open("acacae_adamCRN.pi", "rb"))
-    essential_compounds = ["AMP", "ADP", "ATP", "NAD(+)", "NADH", "NADP(+)", "NADPH", "CTP", "CoA", "H2O", "NH4(+)", "hydrogensulfide"]
-    subgraph = bf_traverse(graph, "D-glucose", essential_compounds)
+def add_reactions(queue: list[any], visited: set[any], reactions: set[any], start_node: any, graph: nx.DiGraph, direction=True):
+    """Adds given reactions to the queue if 
+    the educt nodes are already visited
+    the reaction is not in visited
+    the reaction is not in queue
     
+    queue = The queue the reactions to add on
+    visited = The set of visited nodes
+    start_node = The node we retrieved the reactions from
+    graph = the graph these reactions are in
+    direction = True if correct direciton, False if reversed"""
+    # for each given possible reaction of one compound node
+    for reaction in reactions:
+        # if the current reaction has already been visited 
+        #   (can happen for reversable reaction, example Graph Reaction R2)
+        # if the current reaction has already been added to the queue 
+        #   (can happen if multiple reactions form one product, resulting in same reactions being checked multiple times 
+        #    for product as educt, example Graph Reaction R5 -> 6 -> R3)
+        if reaction in visited or reaction in queue:
+            continue
+        # retrieve label "direction" of the edge between the given compound node and the reaction node
+        direction_label =graph.edges[start_node, reaction]['direction']
+        # initialize missmatch filter
+        found_missmatch = False
+        # for each educt of given reaction
+        for educt in prev_nodes(graph, reaction, direction):
+            # retrieve edge label 'direction'
+            educt_direction_label = graph.edges[educt, reaction]['direction']
+            # if current edge is part of a different direction, ignore it
+            if educt_direction_label != direction_label:
+                continue
+            # if educt is not in visited, reaction cannot be formed (yet)
+            if educt not in visited:
+                found_missmatch = True
+                break
+        # of no missmatch was found, add reaction to queue
+        if found_missmatch is False:
+            queue.append(reaction)
+
+
+def prev_nodes(graph: nx.DiGraph, node: any, direction = True):
+    """Returns list of nodes, that are predecessors of the given node
+    Direction = True means correct direction
+    Direction = False means reversed direction"""
+    if direction:
+        return graph.predecessors(node)
+    else:
+        return graph.successors(node)
+def next_nodes( graph: nx.DiGraph, node, direction = True):
+    """Returns list of nodes, that are successors of the given node
+    Direction = True means correct direction
+    Direction = False means reversed direction"""
+    if direction:
+        return graph.successors(node)
+    else:
+        return graph.predecessors(node)
+
+def read_file(file: str) -> list[str]:
+    """reads in file, splits by new line, trims output."""
+    input = open(file,"r")
+    lines = input.readlines()
+    return [line.strip() for line in lines]
+
+
+
+def main():
+    # read in / generate graph
+    #graph = generate_test_graph()
+    graph = pickle.load(open("acacae_adamCRN.pi", "rb"))    
+    
+    # read in essential compounds
+    essential_compounds = read_file("essential_compounds.txt")
+    
+    # create subgraph via breath first search
+    subgraph = bf_search(graph, "D-glucose", essential_compounds)
+    #subgraph = bf_search(graph, 1, essential_compounds=[1,2,3,19,12,13])
+    
+    # generate position dictionary for graph nodes
     pos = nx.spring_layout(subgraph)
-    nx.draw_networkx(subgraph,pos)
+    
+    # draw network on plot
+    nx.draw(subgraph,pos=pos, with_labels=True)
+    
+    # show plot
     plt.show()
 
 
