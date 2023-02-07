c9b179b6e7c416725c80a185d91b9e15f61720d5
franz <franz.foerster@gmx.de>
2023-02-07 13:50:01 +0100

    added output of generated graphs

--- wp1_script/main.py
+++ wp1_script/main.py
@@ -5,13 +5,16 @@ import pickle
 
 
 def main():
-    datadir = "../../graph_pra"
+    datadir = "../data/crn/"
+    resultdir = "../data/amino_reaction_cycle/"
     for entry in os.scandir(datadir):
         if entry.is_file() and entry.name.endswith(".pi"):
             print(entry.path)
             graph = pickle.load(open(entry.path, "rb"))
             amino_acid_graph = metabolite_subgraph.build_aminoacid_graph(graph)
-
-
+            
+            outfile = resultdir + entry.name.split("/")[-1].replace("CRN", "aa_cycle")
+            pickle.dump(amino_acid_graph, open(outfile, "wb"))
+            
 if __name__ == "__main__":
     main()
