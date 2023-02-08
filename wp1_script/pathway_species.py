c6b483aae175854512a5ddd61d3b0b1b32dcc5f6
Paul Keller <contact.keller.paul@gmail.com>
2023-02-08 13:49:24 +0100

    added pathway_difference comparison

--- wp1_script/pathway_species.py
+++ wp1_script/pathway_species.py
@@ -8,22 +8,47 @@ def main():
     cimIV_species = []
     for entry in os.scandir(datadir):
         if entry.is_file() and entry.name.endswith(".pi"):
-            if "adam" in entry.name:
-                adam_species.append(pickle.load(open(entry.path, "rb")))
-            else:
-                cimIV_species.append(pickle.load(open(entry.path, "rb")))
-    for specie in adam_species:
-        for other_specie in adam_species:
-            if specie != other_specie:
-                compare(specie, other_specie)
+            with open(entry.path, "rb") as reader:
+                graph_object = {'graph': pickle.load(reader), 'name': entry.name}
+                if "adam" in entry.name:
+                    adam_species.append(graph_object)
+                else:
+                    cimIV_species.append(graph_object)
+    result_str = ""
+    for index in range(len(adam_species)):
+        for other_index in range(index+1, len(adam_species)):
+            result_str = result_str + "\n" + compare(adam_species[index], adam_species[other_index], "adam")
+    for index in range(len(cimIV_species)):
+        for other_index in range(index+1, len(cimIV_species)):
+            result_str = result_str + "\n" + compare(cimIV_species[index], cimIV_species[other_index], "cimIV")
+    with open("data/pathway_species/results.txt", "w", encoding="UTF-8") as writer:
+        writer.write(result_str)
 
 def difference(graph: nx.DiGraph, other: nx.DiGraph):
     final: nx.DiGraph = graph.copy()
     final.remove_nodes_from(n for n in graph if n in other)
     return final
 
-def compare(graph, other):
+def compare(graph, other, medium):
+    graph_diff = difference(graph['graph'], other['graph']).nodes(data=True)
+    other_diff = difference(other['graph'], graph['graph']).nodes(data=True)
+    final_graph_nodes = []
+    final_other_nodes = []
+    for node in graph_diff:
+        if 'nodeType' in node[1] and node[1]['nodeType'] == 1:
+            final_graph_nodes.append(node[1]['meta'])
+    for node in other_diff:
+        if 'nodeType' in node[1] and node[1]['nodeType'] == 1:
+            final_other_nodes.append(node[1]['meta'])
     
+    outfile = "data/pathway_species/"+medium+"/"+graph['name']+"_vs_"+other['name']
+    outstring = graph['name']+":\n"+"\n".join(final_graph_nodes)+"\n\n"+other['name']+":\n"+"\n".join(final_other_nodes)
+    with open(outfile, "w", encoding="UTF-8") as writer:
+        writer.write(outstring)
+    return_str = f"{graph['name']} vs {other['name']}: {len(final_graph_nodes)} - {len(final_other_nodes)}"
+    print(return_str)
+    return return_str
 
 if __name__ == "__main__":
     main()
+    
