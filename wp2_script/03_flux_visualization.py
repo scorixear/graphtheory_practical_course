f5cdcf6f8d307a12b32007dfdd3da5189dc9fa68
Kolya Lettl <kolya.lettl@posteo.net>
2023-02-22 13:46:57 +0100

    Refactored main() into run() in flux_visualization

--- wp2_script/03_flux_visualization.py
+++ wp2_script/03_flux_visualization.py
@@ -3,13 +3,9 @@ import pickle
 import os
 import matplotlib.pyplot as plt
 
-fluxDir = "data/flux_results/"
-# aaCycleDir = "data/amino_reaction_cycle/"
-
 
 def build_reaction_graph(G: nx.DiGraph) -> nx.DiGraph:
-    """create a reaction graph consiting only of reactions    
-    """
+    """create a reaction graph consiting only of reactions"""
     reactionGraph = nx.DiGraph()
     reactionData = nx.get_node_attributes(G, "nodeType")
     for reactionNode in G:
@@ -28,7 +24,11 @@ def build_reaction_graph(G: nx.DiGraph) -> nx.DiGraph:
     return reactionGraph
 
 
-def read_file(file: str, fluxDir: str = "data/flux_results/", aaCycleDir: str = "data/amino_reaction_cycle/") -> tuple([dict(), nx.DiGraph()]):
+def read_file(
+    file: str,
+    fluxDir: str = "data/flux_results/",
+    aaCycleDir: str = "data/amino_reaction_cycle/",
+) -> tuple([dict(), nx.DiGraph()]):
 
     flux = pickle.load(open(fluxDir + file, "rb"))
 
@@ -38,46 +38,63 @@ def read_file(file: str, fluxDir: str = "data/flux_results/", aaCycleDir: str = 
     return flux, graph
 
 
-for file in os.listdir(fluxDir):
-    flux, graph = read_file(file)
+def run(fluxDir: str = "data/flux_results/"):
+    for file in os.listdir(fluxDir):
+        flux, graph = read_file(file)
 
-    # select a subgraph from all reactions that are not 0
-    subgraphNodes = set()
-    for node in flux:
-        # filter out irrelevant reactions and export import nodes
-        if flux[node] == 0 or not graph.has_node(node):
-            continue
+        # select a subgraph from all reactions that are not 0
+        subgraphNodes = set()
+        for node in flux:
+            # filter out irrelevant reactions and export import nodes
+            if flux[node] == 0 or not graph.has_node(node):
+                continue
 
-        subgraphNodes.add(node)
-        # find the connected compounds
-        for pre in graph.predecessors(node):
-            subgraphNodes.add(pre)
-        for succ in graph.successors(node):
-            subgraphNodes.add(succ)
+            subgraphNodes.add(node)
+            # find the connected compounds
+            for pre in graph.predecessors(node):
+                subgraphNodes.add(pre)
+            for succ in graph.successors(node):
+                subgraphNodes.add(succ)
 
-    # visualization of the whole graph (including reactions and compounds)
-    aaSynthesisSubgraph = nx.subgraph(graph, subgraphNodes)
-    colours = []
-    for node in aaSynthesisSubgraph:
-        if node in flux:
-            colours.append(flux[node])
-        else:
-            colours.append(0)
+        # visualization of the whole graph (including reactions and compounds)
+        aaSynthesisSubgraph = nx.subgraph(graph, subgraphNodes)
+        colours = []
+        for node in aaSynthesisSubgraph:
+            if node in flux:
+                colours.append(flux[node])
+            else:
+                colours.append(0)
 
-    reactionNodes = [node for node in aaSynthesisSubgraph if aaSynthesisSubgraph.nodes(
-        data=True)[node]["nodeType"] == 1]
-    # nx.draw(aaSynthesisSubgraph, pos=nx.bipartite_layout(aaSynthesisSubgraph, reactionNodes),
-    #        with_labels=True, node_size=20, font_size=4, width=0.3, arrowsize=5, node_color=colours, cmap=plt.cm.viridis)
-    # plt.show()
+        reactionNodes = [
+            node
+            for node in aaSynthesisSubgraph
+            if aaSynthesisSubgraph.nodes(data=True)[node]["nodeType"] == 1
+        ]
+        # nx.draw(aaSynthesisSubgraph, pos=nx.bipartite_layout(aaSynthesisSubgraph, reactionNodes),
+        #        with_labels=True, node_size=20, font_size=4, width=0.3, arrowsize=5, node_color=colours, cmap=plt.cm.viridis)
+        # plt.show()
 
-    # visualize only the reactions
-    reactionSubgraph = build_reaction_graph(aaSynthesisSubgraph)
-    nx.draw(reactionSubgraph, pos=nx.shell_layout(reactionSubgraph),
-            with_labels=True, node_size=20, font_size=4, width=0.3, arrowsize=5, node_color=[flux[node] for node in reactionSubgraph], cmap=plt.cm.viridis)
+        # visualize only the reactions
+        reactionSubgraph = build_reaction_graph(aaSynthesisSubgraph)
+        nx.draw(
+            reactionSubgraph,
+            pos=nx.shell_layout(reactionSubgraph),
+            with_labels=True,
+            node_size=20,
+            font_size=4,
+            width=0.3,
+            arrowsize=5,
+            node_color=[flux[node] for node in reactionSubgraph],
+            cmap=plt.cm.viridis,
+        )
 
-    del reactionSubgraph
-    del graph
-    del aaSynthesisSubgraph
-    outfile = "graphics/" + file.replace("_flux.pi", "_aa_synthesis.png")
-    plt.savefig(outfile, dpi=200)
-    plt.close()
+        del reactionSubgraph
+        del graph
+        del aaSynthesisSubgraph
+        outfile = "graphics/" + file.replace("_flux.pi", "_aa_synthesis.png")
+        plt.savefig(outfile, dpi=200)
+        plt.close()
+
+
+if __name__ == "__main__":
+    run()
